ace6055a17fd8c808819e90ace4ffc79376e3c69
Clawlinker <clawlinker@pawr.link>
2026-03-16 02:21:30 +0000

    🧹 remove dead code: ReceiptCard variants A/B/C/E/ORIGINAL, test-receiptcard-e.tsx, unused BasescanApiResponse type

--- data/types.ts
+++ data/types.ts
@@ -1,18 +1,3 @@
-export interface BasescanApiResponse {
-  status: string
-  message: string
-  result: Array<{
-    hash: string
-    from: string
-    to: string
-    value: string
-    tokenSymbol: string
-    timeStamp: string
-    blockNumber: string
-    tokenDecimal: string
-  }>
-}
-
 export interface BlockscoutApiResponse {
   status: string
   message: string
